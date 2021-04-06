2e81fbf37873c29986b6dfa41a4cfbc96f0b64fb
rloading <r.loading@gmail.com>
2021-04-05 21:25:32 -0300

    correção de rotas e caminho das imagens

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/login.ejs', function(req, res, next) {
+  res.render('login', { title: 'Login' });
+});
+
 module.exports = router;
