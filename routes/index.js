03ce7f190c8d050e42be6aee4df21953212ac5e6
rloading <r.loading@gmail.com>
2021-04-09 17:59:28 -0300

    add footer

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,7 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET login page. */
 router.get('/login.ejs', function(req, res, next) {
   res.render('login', { title: 'Login' });
 });
