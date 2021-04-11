5f544a199827ef9dab0b2730df8150f44b048b7a
rloading <r.loading@gmail.com>
2021-04-11 17:16:15 -0300

    alterações revisão

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,17 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
 });
 
 /* GET login page. */
-router.get('/login.ejs', function(req, res, next) {
-  res.render('login', { title: 'Login' });
+router.get("/login", function (req, res, next) {
+  res.render("login", {
+    title: "Login",
+    isLoginPage: true,
+  });
 });
 
 module.exports = router;
