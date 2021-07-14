aaf954408b6f3d0aad363355c5663ce4723191cb
unknown <r.loading@gmail.com>
2021-07-13 22:33:01 -0300

    correçao estrutura

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,16 +1,24 @@
-var express = require('express');
-var router = express.Router();
-const controller = require('../controllers/pages')
+const express = require("express");
+const router = express.Router();
 
 /* GET home page. */
-router.get('/', controller.home);
-
-/* GET login page. */
-router.get('/login.ejs', function(req, res, next) {
-  res.render('login', { title: 'Login' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
 });
 
-/* GET products page. */
-router.get('/produtos', controller.paginaDeProdutos)
+/* GET login page. */
+router.get("/login", function (req, res, next) {
+  res.render("login", {
+    title: "Login",
+    isLoginPage: true,
+  });
+});
+
+/* GET cadastro page. */
+router.get("/cadastro", function (req, res, next) {
+  res.render("cadastro", {
+    title: "Cadastro",
+  });
+});
 
 module.exports = router;
